ad78bb46412d6f03a0d92f7ecb133cadcea0d17e
Anthony Swierkosz <aswierkosz1@gmail.com>
2022-01-26 02:07:43 -0500

    Starter UI

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,11 @@
+import {MDCTopAppBar} from '@material/top-app-bar';
+
+
 export const run = () => {
   // Time Utility Works
   // TODO Add UI
+
+  // Instantiate Top App Bar
+  const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+  new MDCTopAppBar(topAppBarElement);
 };
