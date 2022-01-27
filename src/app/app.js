d326a6b0652f4d4005b9769db8f4f5cc86f8d159
Anthony Swierkosz <aswierkosz1@gmail.com>
2022-01-26 21:26:06 -0500

    Add basic instantiation and simple test calculation

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,7 @@
 import {MDCTopAppBar} from '@material/top-app-bar';
-
+import {MDCTextField} from '@material/textfield';
+import {MDCRipple} from '@material/ripple';
+import {TimeUtility} from "./utils/time.utility";
 
 export const run = () => {
   // Time Utility Works
@@ -8,4 +10,18 @@ export const run = () => {
   // Instantiate Top App Bar
   const topAppBarElement = document.querySelector('.mdc-top-app-bar');
   new MDCTopAppBar(topAppBarElement);
+
+  const baseTimeField = new MDCTextField(
+      document.querySelector('#base-time-text-field'));
+  const addTimeField = new MDCTextField(
+      document.querySelector('#add-time-text-field'));
+  new MDCRipple(document.querySelector('#calc-button'));
+
+  const calcButton = document.querySelector('#calc-button');
+  const baseTime = document.querySelector('#base-time');
+  const addTime = document.querySelector('#add-time');
+  calcButton.addEventListener('click', () => {
+    console.log(TimeUtility.toTimeString(
+        TimeUtility.add(addTime.value, baseTime.value)));
+  });
 };
