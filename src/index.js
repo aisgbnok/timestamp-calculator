998e170028493b39314bbfaa154c097686c3d3a3
Anthony Swierkosz <aswierkosz1@gmail.com>
2022-02-17 01:18:37 -0500

    Update package dependencies and add Firebase

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import {run} from "./app/app";
 import './styles.scss';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5JTOR1ZVmQegr0xz7cglKcuthy13kcL0",
+  authDomain: "timestamp-calculator.firebaseapp.com",
+  projectId: "timestamp-calculator",
+  storageBucket: "timestamp-calculator.appspot.com",
+  messagingSenderId: "168737895493",
+  appId: "1:168737895493:web:f75ad7e313051ef2771f2c",
+  measurementId: "G-WV9DL3S5X5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 run();
