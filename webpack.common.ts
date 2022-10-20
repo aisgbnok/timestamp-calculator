ac9d0e8e40e85e7f56621527b227575c5755b755
Anthony Swierkosz <anthony@swierkosz.dev>
2022-10-19 22:23:50 -0400

    Move app to TypeScript

--- webpack.common.ts
+++ webpack.common.ts
@@ -10,7 +10,7 @@ export function common(env: any, argv: any) {
   return <Configuration>{
     context: path.resolve(__dirname, 'src'),
     entry: {
-      index: './index.js'
+      index: './index.ts'
     },
     plugins: [
       new MiniCssExtractPlugin({
